0aae514800d932bdf814428c162768f40474788c
Ahmed Kamel <ak@ahmedkamel.net>
2016-08-29 22:58:49 -0700

    convert refresh_token for jwt

--- migrations/2-refresh_token.js
+++ migrations/2-refresh_token.js
@@ -1,10 +1,12 @@
-// var Promise = require('bluebird');
+// var Promise   = require('bluebird')
+//   , randtoken = require('rand-token')
+//   ;
 
-// module.exports.id = "profile";
+// module.exports.id = "refresh_token";
 
 // module.exports.up = function (done) {
 //   // use this.db for MongoDB communication, and this.log() for logging
-//   var coll = this.db.collection('user_account_server');
+//   var coll = this.db.collection('user');
 
 //   var cursor = coll.find();
 //   // Execute the each command, triggers for each document
@@ -14,14 +16,6 @@
 //   cursor.each((err, item) => {
 //     // If the item is null then the cursor is exhausted/empty and closed
 //     if( item == null) {
-//       // Show that the cursor is closed
-//       // cursor.toArray((err, items) => {
-//       //   // console.log(items);
-//       //   // test.equal(null, err);
-
-//       //   // Let's close the db
-//       //   this.db.close();
-//       // });
 //       Promise
 //         .all(waits)
 //         .asCallback(done)
@@ -31,13 +25,12 @@
 //     } else {
 //       var p = Promise
 //                 .fromCallback((cb) => {
-//                   coll.findOneAndUpdate({ _id : item._id }, { $set : { 'id' : item.provider_id } }, cb);
+//                   coll.findOneAndUpdate({ _id : item._id }, { $set : { 'refresh_token' : randtoken.generate(32) } }, cb);
 //                 });
 
 //       waits.push(p);
 //     }
 //   });
-
 // };
 
 // module.exports.down = function (done) {
