9110c1033d727c07d3487883a06787b6b090cab3
Ahmed Kamel <ahmed.kamel@gmail.com>
2018-12-16 22:53:02 -0800

    update config, remove ip filter,

--- config/default.js
+++ config/default.js
@@ -1,23 +1,27 @@
-{
+var bytes = require('bytes')
+  , fs    = require('fs')
+  , ms    = require('ms')
+  ;
+
+module.exports = {
   "jwt"         : {
-    "key"       : "[SECRET]",
-    "public"    : "[PUBLIC]",
-    "expiresIn" : 1800 // 30 * 60 sec
+    "key"       : fs.readFileSync('./.secrets/key/key', 'utf-8'),
+    "public"    : fs.readFileSync('./.secrets/key/key.pub', 'utf-8'),
+    "expiresIn" : ms('30m') / 1000
   },
   "account"     : {
     "port"          : 8050,
-    "ip-whitelist"  : [],
     "impersonate" : {
-      "username"  : null
+      "username"  : "a7medkamel"
     },
     "mongo"       : {
-      "url"     : "mongodb://[USER]:[PASSWORD]@localhost/account"
+      "url"     : `mongodb://${fs.readFileSync('./.secrets/mongodb/username', 'utf-8')}:${fs.readFileSync('./.secrets/mongodb/password', 'utf-8')}@${fs.readFileSync('./.secrets/mongodb/host', 'utf-8')}/account`
     },
     "redis" : {
-      "host"          : "[HOST]",
+      "host"          : "localhost",
       "port"          : 6379,
-      "password"      : "[PASSWORD]",
-      "db"            : 8
+      "password"      : null,
+      "db"            : 0
     },
     "oauth" : {
       "callback"  : "https://www.breadboard.io/auth/callback",
@@ -26,8 +30,8 @@
           "module"        : "passport-github2",
           "name"          : "github.com",
           "options"       : {
-            "clientID"          : "[GITHUB_APP_IP]",
-            "clientSecret"      : "[GITHUB_APP_SECRET]",
+            "clientID"          : fs.readFileSync('./.secrets/oauth/strategy/github.com/id', 'utf-8'),
+            "clientSecret"      : fs.readFileSync('./.secrets/oauth/strategy/github.com/secret', 'utf-8'),
             "authorizationURL"  : null, // "https://github.com/login/oauth/authorize",
             "tokenURL"          : null, // "https://github.com/login/oauth/access_token",
             "customHeaders"     : null, // {},
@@ -47,10 +51,10 @@
   },
   "backend"     : {
       "mongodb"   : {
-        "host"      : "localhost",
+        "host"      : fs.readFileSync('./.secrets/backend/mongodb/host', 'utf-8'),
         "port"      : 27017,
-        "user"      : "[USER]",
-        "password"  : "[PASSWORD]"
+        "user"      : fs.readFileSync('./.secrets/backend/mongodb/user', 'utf-8'),
+        "password"  : fs.readFileSync('./.secrets/backend/mongodb/password', 'utf-8')
       }
   }
-}
+};
